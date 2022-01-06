f225cdbc159bbfc737a9068118a991bec30d1ca8
shikuaizhang <97074570+shikuaizhang@users.noreply.github.com>
2022-01-06 16:40:53 +0800

    Add files via upload
    输出=输入
    按ctrl+z 结束

--- text.c
+++ text.c
@@ -1,32 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	int day = 0;
-scanf_s("%d", &day);
-		switch (day)
-	{
-	case 1:
-		printf("星期1\n");
-		break;
-	case 2:
-		printf("星期2\n");
-		break;
-	case 3:
-		printf("星期3\n");
-		break;
-	case 4:
-		printf("星期4\n");
-		break;
-	case 5:
-		printf("星期5\n");
-		break;
-	case 6:
-		printf("星期6\n");
-		break;
-	case 7:
-		printf("星期7\n");
-		break;
-	
-		}
+int ch = 0;
+while ((ch = getchar()) != EOF)
+putchar(ch);
 	return 0;
 }
